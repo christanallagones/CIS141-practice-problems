009086c02ec90681c2ddc568055b1f3f30c121f6
christanallagones <christancarlallagone@student.olympic.edu>
2025-06-12 14:09:27 -0700

    Update problem2.py

--- Mod2/problem2.py
+++ Mod2/problem2.py
@@ -1,1 +1,17 @@
+# Prompt user for two numbers
+num1 = int(input("Enter your first number"))
+num2 = int(input("Enter your second number"))
+
+# Perform operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# display results
+print(f"\nResults:")
+print(f"{num1} + {num2} = {addition}")
+print(f"{num1} - {num2} = {subtraction}")
+print(f"{num1} * {num2} = {multiplication}")
+print(f"{num1} / {num2} = {division}")
 
