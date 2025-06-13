e0654185b1b307f9fd1c03b38cf688d67a4ca0e0
christanallagones <christancarlallagone@student.olympic.edu>
2025-06-13 16:51:51 -0700

    Update problem1.py

--- Mod3/problem1.py
+++ Mod3/problem1.py
@@ -1,1 +1,6 @@
-
+# Prompt user for word
+word = (input("Enter a word:"))
+# Prompt user for times repeated
+repeat = (int(input("How many times to repeat it?")))
+# Prints word correct amount of times
+print(word * repeat)
