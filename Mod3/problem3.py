71c9d33e6dbfede5ca605c945ec9594610f740ff
christanallagones <christancarlallagone@student.olympic.edu>
2025-06-13 17:18:36 -0700

    Update problem3.py

--- Mod3/problem3.py
+++ Mod3/problem3.py
@@ -1,1 +1,5 @@
+# Prompt the user for a sentence and a word to find in the sentence
+sentence = input("Enter a sentence")
+word = input("Enter a word to find in sentence")
 
+print(word in sentence)
