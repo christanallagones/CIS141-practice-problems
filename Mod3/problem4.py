517b5b878b55df36451486becd555891256a7bd1
christanallagones <christancarlallagone@student.olympic.edu>
2025-06-13 17:40:23 -0700

    Update problem4.py

--- Mod3/problem4.py
+++ Mod3/problem4.py
@@ -1,1 +1,7 @@
+# Prompt user for word, first index, and last index
+word = input("Enter a word:")
+firstindex = int(input("Enter the first index:"))
+lastindex = int(input("Enter the last index:"))
+sliced = word[firstindex:lastindex]
 
+print(sliced)
