be7db9f427be924766e97af482c2115fbdd55262
christanallagones <christancarlallagone@student.olympic.edu>
2025-06-16 20:59:46 -0700

    Update problem1.py

--- Mod4/problem1.py
+++ Mod4/problem1.py
@@ -1,1 +1,6 @@
-
+# Construct a truth table for the expression: (A AND B) OR (NOT B) where A and B each can be True or False. 
+# A B   (A AND B)   (NOT B)    (A AND B) OR (NOT B)
+# T T   T           F           T
+# T F   F           T           T
+# F T   F           F           F
+# F F   F           T           T
