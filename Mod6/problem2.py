638c88bd4bdc7357c9dc65304522708165dbabd0
christanallagones <christancarlallagone@student.olympic.edu>
2025-06-17 00:49:26 -0700

    Update problem2.py

--- Mod6/problem2.py
+++ Mod6/problem2.py
@@ -3,3 +3,6 @@
 olympics = ["Olympic", "College", "Olympic", "Games", "Summer", "Olympics", "Winter", "Olympics"]
 olympic_count = olympics.count("Olympic")
 print(f"The word 'Olympic' appears {olympic_count} times in the list.")
+'''Although there seems to be 4 Olympics in the list, 
+the count will be two because the s at the end of two of them
+I wanted there to be that discretion'''
