7c80f1f1da54ae109cee5e32611e9c6e6904a3ac
christanallagones <christancarlallagone@student.olympic.edu>
2025-06-17 00:54:09 -0700

    Update problem3.py

--- Mod6/problem3.py
+++ Mod6/problem3.py
@@ -1,1 +1,9 @@
-
+'''Create a list of strings. Write code to create a new list that includes 
+only the strings longer than three characters. Print the resulting filtered list.'''
+foods = ["egg", "pizza", "tea", "apple", "rice", "ham", "banana"]
+filtered_list = []
+print(foods)
+for words in foods:
+    if len(words) > 3:
+        filtered_list.append(words)
+print("Food items longer than 3 characters:", filtered_list)
