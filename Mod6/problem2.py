60e75c97ddef0cee566366da398c8287d56010e2
christanallagones <christancarlallagone@student.olympic.edu>
2025-06-17 00:46:42 -0700

    Update problem2.py
    Although there are four Olympics, the count will be two. There is a difference between Olympics and Olympic

--- Mod6/problem2.py
+++ Mod6/problem2.py
@@ -1,1 +1,5 @@
-
+# Create a list of strings. 
+# Write code that counts how many times the word "Olympic" appears in the list, and then print the count.
+olympics = ["Olympic", "College", "Olympic", "Games", "Summer", "Olympics", "Winter", "Olympics"]
+olympic_count = olympics.count("Olympic")
+print(f"The word 'Olympic' appears {olympic_count} times in the list.")
