83f4cc2e7994a9662b0cc41ddb2eb28effaf89dc
christanallagones <christancarlallagone@student.olympic.edu>
2025-06-17 00:03:03 -0700

    Update problem1.py

--- Mod6/problem1.py
+++ Mod6/problem1.py
@@ -1,1 +1,10 @@
-
+# Create a list of integers (you get to pick!). 
+# Write code to iterate through the list and calculate the sum of all even numbers. 
+# Print the resulting sum.
+integers = [2, 6, 15, 9, 26, 41, 37, 19]
+sum_even = 0
+for i in integers:
+    if i % 2 == 0:
+        print(f"{sum_even} + {i}")
+        sum_even += i
+print(f"The sum of all even numbers is: {sum_even}")
